18d344840aa4e2b275353a91c6094cc558837d08
Thinh Nguyen <thinh@vathes.com>
2021-02-23 12:13:56 -0600

    no longer need `get_paramset_idx()` and `get_kilosort_output_directory()`

--- workflow_ephys/pipeline.py
+++ workflow_ephys/pipeline.py
@@ -7,8 +7,6 @@ from elements_lab.lab import Source, Lab, Protocol, User
 
 from .paths import get_ephys_root_data_dir
 from .paths import get_session_directory
-from .paths import get_ks_data_dir as get_kilosort_output_directory
-from .paths import get_paramset_idx
 
 if 'custom' not in dj.config:
     dj.config['custom'] = {}
