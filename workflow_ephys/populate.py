5468229795c3a64ad46811926077a07c55ddd959
Thinh Nguyen <thinh@vathes.com>
2021-03-01 15:39:16 -0600

    `suppress_errors` set to `False` - throw errors for populate calls

--- workflow_ephys/populate.py
+++ workflow_ephys/populate.py
@@ -3,7 +3,7 @@ from workflow_ephys.pipeline import ephys
 
 def populate(display_progress=True):
 
-    populate_settings = {'display_progress': display_progress, 'reserve_jobs': False, 'suppress_errors': True}
+    populate_settings = {'display_progress': display_progress, 'reserve_jobs': False, 'suppress_errors': False}
 
     print('\n---- Populate ephys.EphysRecording ----')
     ephys.EphysRecording.populate(**populate_settings)
