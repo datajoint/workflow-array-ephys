21d25813ce5058b5fa5dba5c498587109b9253e4
Thinh Nguyen <thinh@vathes.com>
2021-03-08 16:21:22 -0600

    minor formatting, updating ERD

--- workflow_ephys/populate.py
+++ workflow_ephys/populate.py
@@ -3,7 +3,9 @@ from workflow_ephys.pipeline import ephys
 
 def populate(display_progress=True):
 
-    populate_settings = {'display_progress': display_progress, 'reserve_jobs': False, 'suppress_errors': False}
+    populate_settings = {'display_progress': display_progress,
+                         'reserve_jobs': False,
+                         'suppress_errors': False}
 
     print('\n---- Populate ephys.EphysRecording ----')
     ephys.EphysRecording.populate(**populate_settings)
