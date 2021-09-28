0df599b0eb131ed9c181cdca9e699a2a8e1f8613
Kabilar Gunalan <kabilar@datajoint.com>
2021-09-28 15:09:47 -0500

    Revert changes

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,7 +20,7 @@ sessions_dirs = ['subject1/session1',
                  'subject2/session1',
                  'subject2/session2',
                  'subject3/session1',
-                 'subject4/session1',
+                 'subject4/experiment1',
                  'subject5/session1',
                  'subject6/session1']
 
@@ -153,9 +153,9 @@ def testdata_paths():
     return {
         'npx3A-p1-ks': 'subject5/session1/probe_1/ks2.1_01',
         'npx3A-p2-ks': 'subject5/session1/probe_2/ks2.1_01',
-        'oe_npx3B-ks': 'subject4/session1/experiment1/recording1/continuous/Neuropix-PXI-100.0/ks',
+        'oe_npx3B-ks': 'subject4/experiment1/recording1/continuous/Neuropix-PXI-100.0/ks',
         'sglx_npx3A-p1': 'subject5/session1/probe_1',
-        'oe_npx3B': 'subject4/session1/experiment1/recording1/continuous/Neuropix-PXI-100.0',
+        'oe_npx3B': 'subject4/experiment1/recording1/continuous/Neuropix-PXI-100.0',
         'sglx_npx3B-p1': 'subject6/session1/towersTask_g0_imec0',
         'npx3B-p1-ks': 'subject6/session1/towersTask_g0_imec0'
     }
