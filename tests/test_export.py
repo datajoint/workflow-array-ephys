1f57a039d05fa61f8bfbe877a600cc0ae1c6b916
Kabilar Gunalan <kabilar@datajoint.com>
2022-03-21 10:09:34 -0500

    Update tests/test_export.py
    Co-authored-by: Chris Brozdowski <CBrozdowski@yahoo.com>

--- tests/test_export.py
+++ tests/test_export.py
@@ -63,7 +63,9 @@ def test_write_to_nwb(pipeline, ingest_lab, ingest_subjects, ingest_sessions, ep
      write_nwb(nwbfile,'/main/test_data/test1.nwb')
 
 
-def test_convert_to_nwb(pipeline, ingest_lab, ingest_subjects, ingest_sessions, ephys_insertionlocation, kilosort_paramset, ephys_recordings, clustering_tasks, clustering, curations):
+def test_convert_to_nwb(pipeline, ingest_lab, ingest_subjects, ingest_sessions, 
+                        ephys_insertionlocation, kilosort_paramset, ephys_recordings, 
+                        clustering_tasks, clustering, curations):
     ephys = pipeline['ephys']
     
     session_key = dict(subject='subject5', session_datetime='2018-07-03 20:32:28')
