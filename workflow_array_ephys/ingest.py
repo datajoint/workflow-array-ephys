bab1281f42fe0fc69ce585a8c811ef30439532f5
Kabilar Gunalan <kabilar@datajoint.com>
2022-03-20 23:57:20 -0500

    Add ingest_lab

--- workflow_array_ephys/ingest.py
+++ workflow_array_ephys/ingest.py
@@ -1,12 +1,47 @@
 import csv
 import re
 
-from workflow_array_ephys.pipeline import subject, ephys, probe, session
+from workflow_array_ephys.pipeline import lab, subject, ephys, probe, session
 from workflow_array_ephys.paths import get_ephys_root_data_dir
 
 from element_array_ephys.readers import spikeglx, openephys
-from element_interface.utils import find_root_directory, find_full_path
+def ingest_lab(lab_csv_path='./user_data/lab/labs.csv',
+               project_csv_path='./user_data/lab/projects.csv',
+               publication_csv_path='./user_data/lab/publications.csv',
+               keyword_csv_path='./user_data/lab/keywords.csv',
+               protocol_csv_path='./user_data/lab/protocols.csv',
+               users_csv_path='./user_data/lab/users.csv',
+               project_user_csv_path='./user_data/lab/project_users.csv',
+               skip_duplicates=True, verbose=True):
+    """
+    Inserts data from a CSVs into their corresponding lab schema tables.
+    By default, uses data from workflow_session/user_data/lab/
+    :param lab_csv_path:      relative path of lab csv
+    :param project_csv_path:  relative path of project csv
+    :param publication_csv_path:     relative path of publication csv
+    :param keyword_csv_path:     relative path of keyword csv
+    :param protocol_csv_path: relative path of protocol csv
+    :param users_csv_path:    relative path of users csv
+    :param project_user_csv_path: relative path of project users csv
+    :param skip_duplicates=True: datajoint insert function param
+    :param verbose: print number inserted (i.e., table length change)
+    """
 
+    # List with repeats for when mult dj.tables fed by same CSV
+    csvs = [lab_csv_path, lab_csv_path,
+            project_csv_path, project_csv_path,
+            publication_csv_path, keyword_csv_path,
+            protocol_csv_path, protocol_csv_path,
+            users_csv_path, users_csv_path, users_csv_path,
+            project_user_csv_path]
+    tables = [lab.Lab(), lab.Location(),
+              lab.Project(), lab.ProjectSourceCode(),
+              lab.ProjectPublication(), lab.ProjectKeywords(),
+              lab.ProtocolType(), lab.Protocol(),
+              lab.UserRole(), lab.User(), lab.LabMembership(),
+              lab.ProjectUser()]
+
+    ingest_csv_to_table(csvs, tables, skip_duplicates=skip_duplicates, verbose=verbose)
 
 def ingest_subjects(subject_csv_path='./user_data/subjects.csv', verbose=True):
     """
