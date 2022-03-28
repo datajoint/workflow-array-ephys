f4d175c4026bd3749ffff4971e4823a501ed3377
Chris Broz <CBroz@datajoint.com>
2022-03-28 17:47:38 -0500

    minor fixes

--- notebooks/py_scripts/08-electrode-localization.py
+++ notebooks/py_scripts/08-electrode-localization.py
@@ -18,13 +18,14 @@
 # Change into the parent directory to find the `dj_local_conf.json` file.
 
 import os
-import datajoint as dj
 # change to the upper level folder to detect dj_local_conf.json
 if os.path.basename(os.getcwd())=='notebooks': os.chdir('..')
 assert os.path.basename(os.getcwd())=='workflow-array-ephys', ("Please move to the "
                                                                + "workflow directory")
+# We'll be working with long tables, so we'll make visualization easier with a limit
+import datajoint as dj; dj.config['display.limit']=10
 
-# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# + [markdown] tags=[] jp-MarkdownHeadingCollapsed=true jp-MarkdownHeadingCollapsed=true tags=[]
 # ## Coordinate Framework
 # -
 
@@ -34,7 +35,7 @@ assert os.path.basename(os.getcwd())=='workflow-array-ephys', ("Please move to t
 # 1. Download the 100μm `nrrd` and `csv` files from the links above.
 # 2. Move these files to your ephys root directory.
 
-# Next, we'll populate the coordinate framework schema simply by loading it.
+# Next, we'll populate the coordinate framework schema simply by loading it. Because we are loading the whole brain volume, this may take 25 minutes or more.
 
 from workflow_array_ephys.localization import coordinate_framework as ccf
 
@@ -42,21 +43,29 @@ from workflow_array_ephys.localization import coordinate_framework as ccf
 
 ccf.BrainRegionAnnotation.BrainRegion()
 
-# Let's look at the dimensions of the primary somatosensory cortex, which has the acronym `SSp1`. To look at other regions, open the CSV you downloaded and search for your desired region.
+# The acronyms listed in the DataJoint table differ slightly from the CCF standard by substituting case-sensitive differences with [snake case](https://en.wikipedia.org/wiki/Snake_case). To lookup the snake case equivalent, use the `retrieve_acronym` function.
 
-ccf.BrainRegionAnnotation.Voxel() & 'acronym="SSp1"'
+central_thalamus = ccf.BrainRegionAnnotation.retrieve_acronym('CM')
+cranial_nerves = ccf.BrainRegionAnnotation.retrieve_acronym('cm')
+print(f'CM: {central_thalamus}\ncm: {cranial_nerves}')
+
+# If your work requires the case-sensitive columns please contact get in touch with the DataJoint team via [StackOverflow](https://stackoverflow.com/questions/tagged/datajoint).
+#
+# For this demo, let's look at the dimensions of the central thalamus. To look at other regions, open the CSV you downloaded and search for your desired region.
+
+cm_voxels = ccf.BrainRegionAnnotation.Voxel() & f'acronym=\"{central_thalamus}\"'
+cm_voxels
+
+cm_x, cm_y, cm_z = cm_voxels.fetch('x', 'y', 'z')
+print(f'The central thalamus extends from \n\tx = {min(cm_x)}  to x = {max(cm_x)}\n\t'
+      + f'y = {min(cm_y)} to y = {max(cm_y)}\n\tz = {min(cm_z)} to z = {max(cm_z)}')
 
 # ## Electrode Localization
 
 # If you have `channel_location` json files for your data, you can look at the position and regions associated with each electrode. Here, we've added an example file to our pre-existing `subject6` for demonstration purposes.
 
-dj.config.load('dj_local_conf.json')
-from workflow_array_ephys.pipeline import subject, session, ephys, probe
+from workflow_array_ephys.localization import coordinate_framework as ccf
 from workflow_array_ephys.localization import electrode_localization as eloc
 
-from workflow_array_ephys.localization import coordinate_framework as ccf
-
-eloc.ElectrodePosition.populate()
+elocBrainRegionAnnotationodePosition.populate()
 eloc.ElectrodePosition.Electrode()
-
-
--- workflow_array_ephys/pipeline.py
+++ workflow_array_ephys/pipeline.py
@@ -10,13 +10,16 @@ from element_animal.subject import Subject
 from element_lab.lab import Source, Lab, Protocol, User, Project
 from element_session.session_with_datetime import Session
 
+from .paths import get_ephys_root_data_dir, get_session_directory
+
 if 'custom' not in dj.config:
     dj.config['custom'] = {}
 
 db_prefix = dj.config['custom'].get('database.prefix', '')
 
 __all__ = ['subject', 'lab', 'session', 'trial', 'event', 'probe', 'ephys',
-           'Subject', 'Source', 'Lab', 'Protocol', 'User', 'Project', 'Session']
+           'Subject', 'Source', 'Lab', 'Protocol', 'User', 'Project', 'Session',
+           'get_ephys_root_data_dir', 'get_session_directory']
 
 # ------------- Import the configured "ephys mode" -------------
 ephys_mode = os.getenv('EPHYS_MODE',
