637e50f73403f8c2acd804426da540f109ee8d14
Chris Broz <CBroz@datajoint.com>
2022-06-23 16:41:48 -0500

    Remove order constraints in test_pipeline_generation

--- tests/test_pipeline_generation.py
+++ tests/test_pipeline_generation.py
@@ -2,19 +2,15 @@ from . import dj_config, pipeline
 
 
 def test_generate_pipeline(pipeline):
-    subject = pipeline['subject']
-    ephys = pipeline['ephys']
-    probe = pipeline['probe']
-    session = pipeline['session']
-
-    subject_tbl, *_ = session.Session.parents(as_objects=True)
+    subject = pipeline["subject"]
+    ephys = pipeline["ephys"]
+    probe = pipeline["probe"]
+    session = pipeline["session"]
 
     # test elements connection from lab, subject to Session
-    assert subject_tbl.full_table_name == subject.Subject.full_table_name
+    assert subject.Subject.full_table_name in session.Session.parents()
 
     # test elements connection from Session to probe, ephys
-    session_tbl, probe_tbl = ephys.ProbeInsertion.parents(as_objects=True)
-    assert session_tbl.full_table_name == session.Session.full_table_name
-    assert probe_tbl.full_table_name == probe.Probe.full_table_name
-    assert 'spike_times' in (ephys.CuratedClustering.Unit.heading.
-                             secondary_attributes)
+    assert session.Session.full_table_name in ephys.ProbeInsertion.parents()
+    assert probe.Probe.full_table_name in ephys.ProbeInsertion.parents()
+    assert "spike_times" in (ephys.CuratedClustering.Unit.heading.secondary_attributes)
