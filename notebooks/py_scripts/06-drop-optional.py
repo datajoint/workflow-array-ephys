6abc0f07da71973ac7954fead372d3ce12e1198d
Kabilar Gunalan <kabilar@datajoint.com>
2022-07-08 12:45:02 -0500

    Update jupytext metadata

--- notebooks/py_scripts/06-drop-optional.py
+++ notebooks/py_scripts/06-drop-optional.py
@@ -1,16 +1,16 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py
+#     formats: ipynb,py_scripts//py
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python [conda env:workflow-ephys]
+#     display_name: Python 3.10.4 64-bit ('python3p10')
 #     language: python
-#     name: conda-env-workflow-ephys-py
+#     name: python3
 # ---
 
 # # Drop schemas
