f96c47ec0dd40e6f506ca020e4e4685a91ef5d21
Chris Broz <CBroz@datajoint.com>
2022-07-21 16:03:31 -0500

    WIP: export fixes for codebook

--- workflow_array_ephys/export.py
+++ workflow_array_ephys/export.py
@@ -2,9 +2,7 @@
 from element_lab.export.nwb import element_lab_to_nwb_dict
 from element_animal.export.nwb import subject_to_nwb
 from element_session.export.nwb import session_to_nwb
-
-# Import ephys NWB export functions
-from element_array_ephys.export.nwb import ecephys_session_to_nwb, write_nwb
+from .pipeline import ephys_mode
 
 __all__ = [
     "element_lab_to_nwb_dict",
@@ -13,3 +11,21 @@ __all__ = [
     "ecephys_session_to_nwb",
     "write_nwb",
 ]
+
+# Import ephys NWB export functions
+if ephys_mode == "no-curation":
+    from element_array_ephys.export.nwb import ecephys_session_to_nwb, write_nwb
+else:
+    missing_ephys_warning = (
+        f"Warning: ephys export requires the no-curation ephys_mode. To use it,\n\t"
+        + "try setting datajoint.config['custom']['ephys_mode'] to 'no-curation'\n\t"
+        + "and restarting your kernel."
+    )
+
+    def ecephys_session_to_nwb(*args):
+        return missing_ephys_warning
+
+    def write_nwb(*args):
+        return missing_ephys_warning
+
+    print(missing_ephys_warning)
