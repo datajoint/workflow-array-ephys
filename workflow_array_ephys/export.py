ade56a91da76a8cb758e2ae905b97f9d10c40a0a
Chris Broz <CBroz@datajoint.com>
2022-07-22 10:46:00 -0500

    Update changelog; Typo in NB 02; remove dummy func from export

--- workflow_array_ephys/export.py
+++ workflow_array_ephys/export.py
@@ -8,24 +8,14 @@ __all__ = [
     "element_lab_to_nwb_dict",
     "subject_to_nwb",
     "session_to_nwb",
-    "ecephys_session_to_nwb",
-    "write_nwb",
 ]
 
 # Import ephys NWB export functions
 if ephys_mode == "no-curation":
     from element_array_ephys.export.nwb import ecephys_session_to_nwb, write_nwb
 else:
-    missing_ephys_warning = (
+    print(
         f"Warning: ephys export requires the no-curation ephys_mode. To use it,\n\t"
         + "try setting datajoint.config['custom']['ephys_mode'] to 'no-curation'\n\t"
         + "and restarting your kernel."
     )
-
-    def ecephys_session_to_nwb(*args):
-        return missing_ephys_warning
-
-    def write_nwb(*args):
-        return missing_ephys_warning
-
-    print(missing_ephys_warning)
