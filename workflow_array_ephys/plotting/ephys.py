287624c5720541426b16c45fc59fc8f2afea7f2c
JR <jaerong.ahn@datajoint.com>
2022-09-30 13:38:33 -0500

    feat: :sparkles: add plotting functions

---
+++ workflow_array_ephys/plotting/ephys.py
@@ -0,0 +1,115 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+from ..pipeline import ephys, probe
+import pandas as pd
+
+
+def plot_driftmap(
+    spike_times: np.ndarray, spike_depths: np.ndarray, colormap="gist_heat_r"
+) -> matplotlib.figure.Figure:
+
+    spike_times = np.hstack(spike_times)
+    spike_depths = np.hstack(spike_depths)
+
+    # Time-depth 2D histogram
+    time_bin_count = 1000
+    depth_bin_count = 200
+
+    spike_bins = np.linspace(0, spike_times.max(), time_bin_count)
+    depth_bins = np.linspace(0, np.nanmax(spike_depths), depth_bin_count)
+
+    spk_count, spk_edges, depth_edges = np.histogram2d(
+        spike_times, spike_depths, bins=[spike_bins, depth_bins]
+    )
+    spk_rates = spk_count / np.mean(np.diff(spike_bins))
+    spk_edges = spk_edges[:-1]
+    depth_edges = depth_edges[:-1]
+
+    # Canvas setup
+    fig = plt.figure(figsize=(12, 5))
+    grid = plt.GridSpec(15, 12)
+
+    ax_cbar = plt.subplot(grid[0, 0:10])
+    ax_driftmap = plt.subplot(grid[2:, 0:10])
+    ax_spkcount = plt.subplot(grid[2:, 10:])
+
+    # Plot main
+    im = ax_driftmap.imshow(
+        spk_rates.T,
+        aspect="auto",
+        cmap=colormap,
+        extent=[spike_bins[0], spike_bins[-1], depth_bins[-1], depth_bins[0]],
+    )
+    # Cosmetic
+    ax_driftmap.invert_yaxis()
+    ax_driftmap.set(
+        xlabel="Time (s)",
+        ylabel="Distance from the probe tip ($\mu$m)",
+        ylim=[depth_edges[0], depth_edges[-1]],
+    )
+
+    # Colorbar for firing rates
+    cb = fig.colorbar(im, cax=ax_cbar, orientation="horizontal")
+    cb.outline.set_visible(False)
+    cb.ax.xaxis.tick_top()
+    cb.set_label("Firing rate (Hz)")
+    cb.ax.xaxis.set_label_position("top")
+
+    # Plot spike count
+    ax_spkcount.plot(spk_count.sum(axis=0) / 10e3, depth_edges, "k")
+    ax_spkcount.set_xlabel("Spike count (x$10^3$)")
+    ax_spkcount.set_yticks([])
+    ax_spkcount.set_ylim(depth_edges[0], depth_edges[-1])
+    sns.despine()
+
+    return fig
+
+
+def plot_waveform(waveform, sampling_rate, ax=None, save_as_dict=False):
+
+    import mpld3
+
+    waveform_df = pd.DataFrame(data={"waveform": waveform})
+    waveform_df["timestamp"] = waveform_df.index / sampling_rate
+    waveform_df.set_index("timestamp", inplace=True)
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=(3, 3))
+    else:
+        save_as_dict = False
+
+    ax.plot(waveform_df, "k")
+    ax.set(xlabel="Time (ms)", ylabel="Voltage ($\mu$V)", title="Avg. waveform")
+    sns.despine()
+    if save_as_dict:
+        fig_dict = mpld3.fig_to_dict(fig)
+        plt.close()
+        return fig_dict
+
+
+def plot_correlogram(spike_times, bin_size=0.001, window_size=1, ax=None):
+
+    from brainbox.singlecell import acorr
+
+    correlogram = acorr(
+        spike_times=spike_times, bin_size=bin_size, window_size=window_size
+    )
+    df = pd.DataFrame(
+        data={"correlogram": correlogram},
+        index=pd.RangeIndex(
+            start=-(window_size * 1e3) / 2,
+            stop=(window_size * 1e3) / 2 + bin_size * 1e3,
+            step=bin_size * 1e3,
+        ),
+    )
+    df["lags"] = df.index  # in ms
+    ax.plot(df["lags"], df["correlogram"], color="royalblue", linewidth=0.5)
+    ymax = round(correlogram.max() / 10) * 10
+    ax.set_ylim(0, ymax)
+    # ax.axvline(x=0, color="k", linewidth=0.5, ls="--")
+    ax.set(xlabel="Lags (ms)", ylabel="Count", title="Auto Correlogram")
+    sns.despine()
+
+    return ax
